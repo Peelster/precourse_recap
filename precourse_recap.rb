d160441ed7c7569e56ef6636f8ada36f2d1e651e
Peeltser <celiaduncan01@gmail.com>
2020-02-17 20:20:17 +0000

    adding homework

---
+++ precourse_recap.rb
@@ -0,0 +1,24 @@
+weather = "raining"
+if weather == "raining"
+  puts "take brolly"
+else "take sunglasses"
+end
+
+def greet
+  p "hello"
+  end
+greet
+
+
+animals = ["cat","dog","mouse"]
+puts animals
+
+sound_module = {"type" => "granular", "name" => "clouds", "age" => 2 }
+puts sound_module
+
+counter = 0
+target = 5
+while counter <= target
+  puts "{#{counter}}"
+  counter = 0+1
+end
